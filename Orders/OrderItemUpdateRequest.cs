5cbfc304ce055ac3620d6016f98dbde219324acf
Jonathan Price <92233926+JonathanPrice95@users.noreply.github.com>
2022-01-17 10:28:11 -0800

    Add files via upload

---
+++ Orders/OrderItemUpdateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sabio.Models.Requests.Orders
+{
+    public class OrderItemUpdateRequest : OrderItemAddRequest, IModelIdentifier
+    {
+        public int Id { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int InventoryId { get; set; }
+    }
+}
